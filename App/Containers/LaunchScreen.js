961ab66b8f8d37c018832e00e038334732812f8d
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-29 22:45:58 +0500

    Feed camera into WebGL view added

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -3,6 +3,7 @@ import {Screen} from '@shoutem/ui'
 import Camera from 'react-native-camera';
 import { Surface } from "gl-react";
 // Styles
+import Saturate from './Saturation'
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
@@ -13,29 +14,54 @@ export default class LaunchScreen extends Component {
       height: null
     }
   }
-  onLayout= (ev) =>{
+  componentWillUpdate() {
+    clearInterval(this.timer);
+  }
+  refreshPic = () =>{
+    this.camera.capture({
+      target: Camera.constants.CaptureTarget.temp,
+      jpegQuality: 70
+    }).then( data => this.setState({
+      path: data.path
+    })).catch( err => console.error (err));
+  }
+  start(){
+    this.timer = setInterval(()=> this.refreshPic(), 5)
+  }
+  onLayout = (ev) =>{
     const {width, height} = ev.nativeEvent.layout;
     this.setState({
       width,  height
     });
-  }  
+    this.start()
+  }
   render () {
     const {width, height} = this.state;
-    return (
-      
-      <Screen onLayout={this.onLayout}>
-        <Camera style={{flex: 1}}
-                ref={cam => this.camera = cam}
-                aspect={Camera.constants.Aspect.fill}>
-          <Surface style={{width, height}}>
-                <Saturate {...filter}>
-                  {{uri: 'http://i.imgur.com/uTp9Xfr.jpg'}}
-                </Saturate>
-                
-              </Surface>
-        </Camera>  
-
-      </Screen>
-    )
+    const filter = {
+      contrast: 1,
+      saturation: 1,
+      brightness: 1
+    }
+    if (width && height) {
+      return (    
+        <Screen onLayout={this.onLayout}>
+          <Camera style={{flex: 1}}
+                  ref={cam => this.camera = cam}
+                  aspect={Camera.constants.Aspect.fill}>
+            <Surface style={{width, height}}>
+              
+              <Saturate {...filter}>
+                {{uri: 'http://i.imgur.com/uTp9Xfr.jpg'}}
+              </Saturate>
+                  
+            </Surface>
+          </Camera>  
+        </Screen>
+      )
+    }else{
+      return (
+          <Screen onLayout={this.onLayout} />
+      );
+    }
   }
 }
---
+++ App/Containers/Saturation.js
@@ -0,0 +1,30 @@
+//@flow
+import React, { Component } from "react";
+import { Shaders, Node, GLSL } from "gl-react";
+// import { Surface } from "gl-react-dom";
+
+const shaders = Shaders.create({
+  Saturate: {
+    frag: `
+precision highp float;
+varying vec2 uv;
+uniform sampler2D t;
+uniform float contrast, saturation, brightness;
+const vec3 L = vec3(0.2125, 0.7154, 0.0721);
+void main() {
+  vec4 c = texture2D(t, uv);
+	vec3 brt = c.rgb * brightness;
+	gl_FragColor = vec4(mix(
+    vec3(0.5),
+    mix(vec3(dot(brt, L)), brt, saturation),
+    contrast), c.a);
+}
+`
+  }
+});
+
+export const Saturate = ({ contrast, saturation, brightness, children }) =>
+  <Node
+    shader={shaders.Saturate}
+    uniforms={{ contrast, saturation, brightness, t: children }}
+  />;
