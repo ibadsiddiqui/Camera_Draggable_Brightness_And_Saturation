b744de5e5df6a9245711f13e84c2447d10cd5647
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-29 13:57:18 +0500

    camera changes added

---
+++ App/Containers/CameraScreen.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {Screen} from '@shoutem/ui'
+import Camera from 'react-native-camera';
+// Styles
+import styles from './Styles/LaunchScreenStyles'
+
+export default class LaunchScreen extends Component {
+  constructor(){
+    super();
+    this.state = {
+      width: null,
+      height: null
+    }
+  }
+  onLayout(ev){
+    const {width, height} = ev.nativeEvent.layout;
+    this.setState({
+      width,
+      height
+    });
+  }  
+  render () {
+    const {width, height} = this.state;
+    return (
+      
+      <Screen onLayout={this.onLayout}>
+        <Camera style={{flex: 1}}
+                ref={cam => this.camera = cam}
+                aspect={Camera.constants.Aspect.fill}>
+        </Camera>  
+
+      </Screen>
+    )
+  }
+}
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,13 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import CameraScreen from '../Containers/CameraScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  CameraScreen: { screen: CameraScreen}
 }, {
   // Default config for all screens
   headerMode: 'none',
